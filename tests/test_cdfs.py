0d16e1b71a9cb624fe9f624c1300bd9bb8c5ff32
mvds314 <postaanmartin@gmail.com>
2023-10-17 22:02:59 +0200

    basic cgf pdf test

--- tests/test_cdfs.py
+++ tests/test_cdfs.py
@@ -3,6 +3,9 @@
 
 import numpy as np
 import pandas as pd
+import scipy as sp
+import scipy.stats as sps
+from scipy.integrate import quad
 
 import pytest
 import warnings
@@ -12,8 +15,9 @@ from spapprox import cumulant_generating_function, norm
 
 def test_norm_cgf():
     ncgf = norm(mu=0, sigma=1)
+    cgf = lambda t: np.log(quad(lambda x: sps.norm.pdf(x) * np.exp(t * x), a=-10, b=10)[0])
     assert isinstance(ncgf, cumulant_generating_function)
-    assert True
+    assert np.isclose(cgf(1), ncgf.K(1))
 
 
 if __name__ == "__main__":
