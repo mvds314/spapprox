589fe5bc7eb9b36a8c96bddbfa855fbc43b6c324
mvds314 <postaanmartin@gmail.com>
2023-11-02 22:01:03 +0100

    test fit functions normal dist

--- tests/test_spa.py
+++ tests/test_spa.py
@@ -47,10 +47,23 @@ def test_norm_spa(cgf, dist, trange):
     for t in [-2, -1, 1 / 6]:
         x = spa.cgf.dK(t)
         assert np.isclose(spa.cgf.dK(spa._dK_inv(x)), x, atol=1e-3)
-    # Test investion ppf
-    for q in [0.05, 0.1, 0.3, 0.5, 0.9, 0.95]:
-        np.isclose(spa.cdf(x=spa.ppf(q)), q, atol=1e-4)
-    # TODO: put some tests here
+    # Test clear cache
+    assert hasattr(spa, "_x_cache") and hasattr(spa, "_t_cache")
+    spa.clear_cache()
+    assert not hasattr(spa, "_x_cache") and not hasattr(spa, "_t_cache")
+    # Test cdf
+    qs = [0.05, 0.1, 0.3, 0.5, 0.9, 0.95]
+    for q in qs:
+        # Note: It's better compare x than p
+        assert np.isclose(dist.ppf(spa.cdf(x=dist.ppf(q))), dist.ppf(q), atol=5e-2)
+    for q in qs:
+        assert np.isclose(spa.cdf(x=spa.ppf(q)), q, atol=1e-6)
+    assert np.allclose(spa.cdf(x=spa.ppf(qs)), qs, atol=1e-4)
+    # Same tests but then with ppf fitted
+    spa.fit_ppf()
+    for q in qs:
+        assert np.isclose(spa.cdf(x=spa.ppf(q)), q, atol=1e-3)
+    assert np.allclose(spa.cdf(x=spa.ppf(qs)), qs, atol=1e-3)
 
 
 @pytest.mark.parametrize(
@@ -146,9 +159,6 @@ def test_expon_spa(cgf, dist, trange):
     for q in qs:
         assert np.isclose(spa.cdf(x=spa.ppf(q)), q, atol=1e-3)
     assert np.allclose(spa.cdf(x=spa.ppf(qs)), qs, atol=1e-3)
-    # assert np.allclose(spa.cdf(x=spa.ppf(qs)), qs, atol=1e-4)
-    # assert ~np.allclose(spa.ppf(qs), spa2.ppf(qs), atol=1e-4)
-    # TODO: test fit saddle point approximation
 
 
 if __name__ == "__main__":
@@ -156,8 +166,8 @@ if __name__ == "__main__":
         pytest.main(
             [
                 str(Path(__file__)),
-                # "-k",
-                # "test_expon_spa",
+                "-k",
+                "test_expon_spa",
                 "--tb=auto",
                 "--pdb",
             ]
