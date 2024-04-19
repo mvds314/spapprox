cde7355b90da4d619ae592dca1387084a0e8dc8b
mvds314 <postaanmartin@gmail.com>
2024-04-19 11:48:30 +0200

    Fix imports

--- spapprox/__init__.py
+++ spapprox/__init__.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from .cgf_base import (
-    MultivariateCumulantGeneratingFunction,  # noqa: F401
-    UnivariateCumulantGeneratingFunction,  # noqa: F401
+from .cgf_base import (  # noqa: F401
+    MultivariateCumulantGeneratingFunction,
+    UnivariateCumulantGeneratingFunction,
 )
 
 # noqa: F401
-from .cgfs import (  # noqa: F401  # noqa: F401  # noqa: F401
+from .cgfs import (  # noqa: F401
     binomial,
     chi2,
     exponential,
@@ -20,5 +20,10 @@ from .cgfs import (  # noqa: F401  # noqa: F401  # noqa: F401
     univariate_sample_mean,
 )
 from .domain import Domain  # noqa: F401
-from .spa import UnivariateSaddlePointApprox, UnivariateSaddlePointApproxMean  # noqa: F401
+from .spa import (  # noqa: F401
+    UnivariateSaddlePointApprox,
+    UnivariateSaddlePointApproxMean,
+    MultivariateSaddlePointApprox,
+    BivariateSaddlePointApprox,
+)
 from .util import Timer  # noqa: F401
