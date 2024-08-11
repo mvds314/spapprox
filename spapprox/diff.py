Fix gradient

spapprox/diff.py
@@ -73,11 +73,6 @@ class FindiffBase(ABC):
                 ), "f is assumed to be scalar, 3 retval are expected when feeding t-h, t and t+h"
                 if not np.isnan(fxx).any():
                     continue
-                import pdb
-
-                pdb.set_trace()
-                # TODO: I'm confused about the shape of x, shouldn't it be transposed?
-                # TODO: continue here and check this logic
                 assert not np.isnan(fxx[1]).any(), "Domain is assumed to be rectangular"
                 if np.isnan(fxx[0]).any():
                     # Shift to the right
@@ -93,8 +88,10 @@ class FindiffBase(ABC):
                     x[i] += -self.h
                 else:
                     raise RuntimeError("This should never happen, all cases should be handled")
-                assert not np.isnan(fxx).any(), "Shifts are assumed to fix any domain issues"
-            # TODO: test if the above logic works
+                assert not np.isnan(
+                    self.f(x[i])
+                ).any(), "Shifts are assumed to fix any domain issues"
+            assert not np.isnan(self.f(x.T)).any(), "Shifts are assumed to fix any domain issues"
         return (
             np.array(np.meshgrid(*[x[i] for i in range(self.dim)], indexing="ij"))
             .reshape((self.dim, 3**self.dim))

tests/test_diff.py
@@ -11,23 +11,22 @@ from spapprox.diff import Gradient
 @pytest.mark.parametrize(
     "f, df, dim, h, points",
     [
-        # (
-        #     lambda x: np.sum(np.square(x), axis=-1),
-        #     lambda x: 2 * x,
-        #     2,
-        #     1e-6,
-        #     np.array([np.linspace(0, 1, 10), np.linspace(0, 1, 10)]).T,
-        # ),
-        # (
-        #     lambda x: np.where(np.all(x > 0, axis=-1), np.sum(np.square(x), axis=-1), np.nan),
-        #     lambda x: np.where(
-        #         np.tile(np.all(x > 0, axis=-1), (2, 1)).T, 2 * x, np.nan * np.ones_like(x)
-        #     ).squeeze(),
-        #     2,
-        #     1e-6,
-        #     np.array([np.linspace(0, 1, 10), np.linspace(0, 1, 10)]).T,
-        # ),
-        # TODO: fix this unittest
+        (
+            lambda x: np.sum(np.square(x), axis=-1),
+            lambda x: 2 * x,
+            2,
+            1e-6,
+            np.array([np.linspace(0, 1, 10), np.linspace(0, 1, 10)]).T,
+        ),
+        (
+            lambda x: np.where(np.all(x > 0, axis=-1), np.sum(np.square(x), axis=-1), np.nan),
+            lambda x: np.where(
+                np.tile(np.all(x > 0, axis=-1), (2, 1)).T, 2 * x, np.nan * np.ones_like(x)
+            ).squeeze(),
+            2,
+            1e-6,
+            np.array([np.linspace(0, 1, 10), np.linspace(0, 1, 10)]).T,
+        ),
         (
             lambda x: np.where(np.all(x >= 0, axis=-1), np.sum(np.square(x), axis=-1), np.nan),
             lambda x: np.where(
@@ -37,8 +36,15 @@ from spapprox.diff import Gradient
             1e-6,
             np.array([np.linspace(0, 1, 10), np.linspace(0, 1, 10)]).T,
         ),
-        # TODO: add a test with a >= domain so that we get a border case
-        # TODO: what other cases to test?
+        (
+            lambda x: np.where(np.all(x <= 0, axis=-1), np.sum(np.square(x), axis=-1), np.nan),
+            lambda x: np.where(
+                np.tile(np.all(x <= 0, axis=-1), (2, 1)).T, 2 * x, np.nan * np.ones_like(x)
+            ).squeeze(),
+            2,
+            1e-6,
+            np.array([np.linspace(-1, 0, 10), np.linspace(-1, 0, 10)]).T,
+        ),
     ],
 )
 def test_grad(f, df, dim, h, points):