1610b5c175e4391ad44eb6ea092b84c9bd9e6776
mvds314 <postaanmartin@gmail.com>
2024-08-11 13:18:25 +0200

    Add Partial derivative test

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 import numpy as np
 import pytest
-from spapprox.diff import Gradient
+from spapprox.diff import Gradient, PartialDerivative
 
 
 @pytest.mark.parametrize(
@@ -56,7 +56,52 @@ def test_grad(f, df, dim, h, points):
     assert np.allclose(grad(points), df(points), equal_nan=True)
 
 
-# TODO: test higher order derivatives
+@pytest.mark.parametrize(
+    "f, df, dim, h, points",
+    [
+        (
+            lambda x: np.sum(np.square(x), axis=-1),
+            lambda x: 2 * x,
+            2,
+            1e-6,
+            np.array([np.linspace(0, 1, 10), np.linspace(0, 1, 10)]).T,
+        ),
+        (
+            lambda x: np.where(np.all(x > 0, axis=-1), np.sum(np.square(x), axis=-1), np.nan),
+            lambda x: np.where(
+                np.tile(np.all(x > 0, axis=-1), (2, 1)).T, 2 * x, np.nan * np.ones_like(x)
+            ).squeeze(),
+            2,
+            1e-6,
+            np.array([np.linspace(0, 1, 10), np.linspace(0, 1, 10)]).T,
+        ),
+        (
+            lambda x: np.where(np.all(x >= 0, axis=-1), np.sum(np.square(x), axis=-1), np.nan),
+            lambda x: np.where(
+                np.tile(np.all(x >= 0, axis=-1), (2, 1)).T, 2 * x, np.nan * np.ones_like(x)
+            ).squeeze(),
+            2,
+            1e-6,
+            np.array([np.linspace(0, 1, 10), np.linspace(0, 1, 10)]).T,
+        ),
+        (
+            lambda x: np.where(np.all(x <= 0, axis=-1), np.sum(np.square(x), axis=-1), np.nan),
+            lambda x: np.where(
+                np.tile(np.all(x <= 0, axis=-1), (2, 1)).T, 2 * x, np.nan * np.ones_like(x)
+            ).squeeze(),
+            2,
+            1e-6,
+            np.array([np.linspace(-1, 0, 10), np.linspace(-1, 0, 10)]).T,
+        ),
+    ],
+)
+def test_partial_derivative():
+    pass
+    if True:
+        pass
+    # TODO: first test equivalance partial derivative and the gradient
+    # TODO: test higher order derivatives
+
 
 if __name__ == "__main__":
     if True:
