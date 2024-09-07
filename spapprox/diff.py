Partial derivative bug fixes

spapprox/diff.py
@@ -40,13 +40,13 @@ class FindiffBase(ABC):
         raise NotImplementedError
 
     @property
-    def _h_vect(self, t):
+    def _h_vect(self):
         """
         Return the vector of h's
         """
         if not hasattr(self, "_h_vect_cache"):
             h = np.asanyarray(self.h)
-            if h.dim > 0:
+            if h.ndim > 0:
                 assert len(h) == self.dim, "h should be a scalar or a vector of length dim"
                 self._h_vect_cache = h
             else:

tests/test_diff.py
@@ -97,10 +97,10 @@ def test_partial_derivative(f, df, dim, h, points):
     for i in range(dim):
         orders = [0] * dim
         orders[i] = 1
-        gradi = PartialDerivative(f, *orders, h=h)
+        pdi = PartialDerivative(f, *orders, h=h)
         for p in points:
-            assert np.allclose(gradi(p), df(p)[i], atol=1e-6, equal_nan=True)
-        assert np.allclose(gradi(points), df(points)[i], equal_nan=True)
+            assert np.allclose(pdi(p), df(p)[i], atol=1e-6, equal_nan=True)
+        assert np.allclose(pdi(points), df(points)[i], equal_nan=True)
 
 
 # TODO: first test equivalance partial derivative and the gradient