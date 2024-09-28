3c12b32b62e79f2693c0757b85306c71a1ba3e93
mvds314 <postaanmartin@gmail.com>
2024-09-28 16:55:02 +0200

    Adjust diff.py to support both scalar and 1 dim

--- spapprox/diff.py
+++ spapprox/diff.py
@@ -68,26 +68,42 @@ class FindiffBase(ABC):
         """
         if not hasattr(self, "_h_vect_cache"):
             h = np.asanyarray(self.h)
-            if h.ndim > 0:
+            if self.dim == 0:
+                assert h.ndim == 0, "h should be scalar if for scalar valued input"
+                self._h_vect_cache = self.h
+            elif h.ndim == 1:
                 assert len(h) == self.dim, "h should be a scalar or a vector of length dim"
                 self._h_vect_cache = h
-            else:
+            elif h.ndim == 0:
                 self._h_vect_cache = np.array([self.h] * self.dim)
+            else:
+                raise AssertionError("h can have at most 1 dimension")
         return self._h_vect_cache
 
-    def _build_grid(self, t):
+    def _build_grid(self, t, dim=None):
+        # Initialize
+        if dim is None:
+            dim = self.dim
         t = np.asanyarray(t)
-        assert len(t) == self.dim, "Dimension does not match"
-        assert not np.isnan(self.f(t)), "t should be in the domain"
+        assert t.ndim == self.dim == 0 or len(t) == self.dim, "Dimensions do not match"
+        t = np.atleast_1d(t)  # Make the below work for vector valued input
+        assert not np.isnan(self.f(t)).any(), "t should be in the domain"
+        # Handle dim=0 case by handling at as a 1-dim vector case
+        if dim == 0:
+            # TODO: check return values -> maybe we need to squeeze something
+            grid, sel = self._build_grid(t, dim=1)
+            return np.sqeeze(grid), sel
+        # Continue with the dim>=1 case
+        assert dim > 1, "Domain is assumed to be a vector space at this point"
         # Use central differences by default
         x = np.array([t - self.h, t, t + self.h]).T
-        sel = [1] * self.dim
+        sel = [1] * dim
         # But adjust if t-h or t+h is not in the domain (for any of the components)
         if np.isnan(self.f(x)).any():
             assert not np.isnan(self.f(np.zeros_like(t))), "Zeros is asserted to be in the domain"
             # Because of the rectangular domain?
-            for i in range(self.dim):
-                xx = np.zeros((3, self.dim))
+            for i in range(dim):
+                xx = np.zeros((3, dim))
                 xx[:, i] = x[i]
                 fxx = self.f(xx)
                 assert (
@@ -115,23 +131,22 @@ class FindiffBase(ABC):
                 ).any(), "Shifts are assumed to fix any domain issues"
             assert not np.isnan(self.f(x.T)).any(), "Shifts are assumed to fix any domain issues"
         return (
-            np.array(np.meshgrid(*[x[i] for i in range(self.dim)], indexing="ij"))
-            .reshape((self.dim, 3**self.dim))
+            np.array(np.meshgrid(*[x[i] for i in range(dim)], indexing="ij"))
+            .reshape((dim, 3**dim))
             .T
         ), sel
 
-    # TODO: this implementation seems inconsistent
-    # TODO: why woudl we squeeze? This should be a propoerty of the implemented function
     def __call__(self, t):
         t = np.asanyarray(t)
+        assert (
+            t.ndim <= 2
+        ), "Only scalar, vector, or vectorized, as in lists with scalars or lists with vectors, evaluations are supported"
         # Handle vectorized evaluation
         if t.ndim == 2:
-            return np.array([np.squeeze(self(tt)) for tt in t])
-        # Process input
-        assert t.ndim == 1, "Only vector or list of vector evaluations are supported"
-        assert len(t) == self.dim, "Dimension does not match"
-        if np.isnan(self.f(t)):
-            retval = np.full(self.dim_image, np.nan)
+            return np.array([self(tt) for tt in t])
+        assert t.ndim == self.dim == 0 or len(t) == self.dim, "Dimensions do not match"
+        if np.isnan(self.f(t)).any():
+            retval = np.nan if self.dim_image == 0 else np.full(self.dim_image, np.nan)
         else:
             Xis, sel = self._build_grid(t)
             retval = self.f(Xis).reshape(tuple([3] * self.dim))
@@ -177,9 +192,7 @@ class Gradient(FindiffBase):
     @property
     def _findiff(self):
         if not hasattr(self, "_findiff_cache"):
-            self._findiff_cache = fd.Gradient(
-                h=[self.h] * self.dim if np.isscalar(self.h) else self.h
-            )
+            self._findiff_cache = fd.Gradient(h=self._h_vect)
         return self._findiff_cache
 
 
@@ -219,9 +232,9 @@ class PartialDerivative(FindiffBase):
     @property
     def dim_image(self):
         """
-        Dimension of the image of the partial derivative
+        Dimension of the image of the partial derivative is scalar valued
         """
-        return 1
+        return 0
 
     @property
     def _findiff(self):
@@ -234,9 +247,8 @@ class PartialDerivative(FindiffBase):
             )
         return self._findiff_cache
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, t, *args, **kwargs):
         # TODO: this should work for first derivative, probably we need more grid points for higher ones
-        # TODO: add  check
         assert (
             np.max(self.orders) <= 1
         ), "Only first order derivatives are implemented, grid should be extended for higher order"
