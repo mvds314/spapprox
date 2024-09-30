3bd60a1bedba4562398a7d357616d0390d445a9d
mvds314 <postaanmartin@gmail.com>
2024-09-30 19:51:33 +0200

    More tests scalar diff case

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -98,6 +98,7 @@ def test_grad(f, df, dim, h, points, error):
 @pytest.mark.parametrize(
     "f, df, ndim, h, points, error",
     [
+        # Tests for the vector case
         (
             lambda x: np.sum(np.square(x), axis=-1),
             lambda x: 2 * x,
@@ -106,6 +107,7 @@ def test_grad(f, df, dim, h, points, error):
             np.array([np.linspace(0, 1, 10), np.linspace(0, 1, 10)]).T,
             None,
         ),
+        # Vector case with different domain constraints
         (
             lambda x: np.where(np.all(x > 0, axis=-1), np.sum(np.square(x), axis=-1), np.nan),
             lambda x: np.where(
@@ -136,12 +138,43 @@ def test_grad(f, df, dim, h, points, error):
             np.array([np.linspace(-1, 0, 10), np.linspace(-1, 0, 10)]).T,
             None,
         ),
+        # Tests for the scalar case
         (
             lambda x: np.sum(np.square(x), axis=-1),
             lambda x: 2 * x,
             1,
             1e-6,
             np.linspace(0, 1, 10),
+            None,
+        ),
+        (
+            lambda x: np.where(np.asanyarray(x) > 0, np.square(x), np.nan),
+            lambda x: np.where(
+                np.asanyarray(x) > 0, 2 * np.asanyarray(x), np.nan * np.ones_like(x)
+            ),
+            1,
+            1e-6,
+            np.linspace(0, 1, 10),
+            None,
+        ),
+        (
+            lambda x: np.where(np.asanyarray(x) >= 0, np.square(x), np.nan),
+            lambda x: np.where(
+                np.asanyarray(x) >= 0, 2 * np.asanyarray(x), np.nan * np.ones_like(x)
+            ),
+            1,
+            1e-6,
+            np.linspace(0, 1, 10),
+            None,
+        ),
+        (
+            lambda x: np.where(np.asanyarray(x) <= 0, np.square(x), np.nan),
+            lambda x: np.where(
+                np.asanyarray(x) <= 0, 2 * np.asanyarray(x), np.nan * np.ones_like(x)
+            ),
+            1,
+            1e-6,
+            np.linspace(-1, 0, 10),
             None,
         ),
     ],
@@ -160,7 +193,7 @@ def test_partial_derivative(f, df, ndim, h, points, error):
                 pdi = PartialDerivative(f, *orders, h=h)
             for p in points:
                 dfp = df(p)
-                dfpi = dfp if np.isscalar(dfp) else dfp[i]
+                dfpi = dfp if np.isscalar(dfp) or dfp.ndim == 0 else dfp[i]
                 pdip = pdi(p)
                 assert np.asanyarray(p).ndim <= 1, "Invalid test case"
                 assert np.isscalar(pdip)
@@ -184,7 +217,7 @@ def test_partial_derivative(f, df, ndim, h, points, error):
                 pdi(points)
 
 
-# TODO: add more tests for scalar case
+# TODO: add some tests that actually raise an error
 
 # TODO: test equivalance partial derivative and the gradient
 
