41884ac2347c53d9b8889d36d11970ab1fe76876
mvds314 <postaanmartin@gmail.com>
2025-01-24 11:46:46 +0100

    Fix test

--- tests/test_mvar_cgfs.py
+++ tests/test_mvar_cgfs.py
@@ -427,10 +427,9 @@ def test_multiplication_and_division(mcgf1, mcgf2, ts, dim):
             MultivariateCumulantGeneratingFunction.from_univariate(
                 2 * gamma(1.1) + gamma(1.2) + gamma(1.3)
             ),
-            [-0.1, -0.2, 0, 0.2, 0.4],
+            [[-0.1], [-0.2], [0], [0.2], [0.4]],
             None,
             id="Project 2D gamma on 1D",
-            marks=[pytest.mark.xfail, pytest.mark.tofix],
         ),
         pytest.param(
             multivariate_norm(loc=np.zeros(2), scale=1).ldot(np.atleast_2d(np.ones(2))),
@@ -439,7 +438,6 @@ def test_multiplication_and_division(mcgf1, mcgf2, ts, dim):
             None,
             id="Project 3D normal on 1D",
         ),
-        # TODO: fix failing tests
     ],
 )
 def test_ldot(mcgf1, mcgf2, ts, dim):
