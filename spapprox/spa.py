7e09c14191991ae640ada676afdb51ea2cc667bc
mvds314 <mvds314@users.noreply.github.com>
2025-01-25 14:17:50 +0100

    Refactor

--- spapprox/spa.py
+++ spapprox/spa.py
@@ -685,10 +685,15 @@ class BivariateSaddlePointApprox(MultivariateSaddlePointApprox):
     References
     ----------
     [1] Broda, Paolella (2011) - Saddlepoint approximations - a review
+
     [2] Butler, R. W. (2007). Saddlepoint approximations with applications.
+
     [3] Wang (1990) - Saddlepoint approximations for bivariate distributions
+
     [4] Paolella (2007) - Intermediate Probability
+
     [5] Kolassa (2006) - Serie approximations in statistics
+
     [6] Kolassa, Li (2011) - Series approximations to multivariate saddlepoint approximations
 
     """
